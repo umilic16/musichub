482e85b71d7dc131af0a94b61b3f9a72e540747d
umilic16 <urke.1998ds@gmail.com>
2023-03-20 22:52:00 +0100

    ner init

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 from functions import get_music_info, play_song
-from training.neuralintents import GenericAssistant
+from intent_recognition.neuralintents import GenericAssistant
+import spacy
+from spacy import displacy
+
+nlp = spacy.load("en_core_web_trf")
+
+def named_entity_recoqnition(message):
+    doc = nlp(message)
+    print(doc.ents)
+    # displacy.serve(doc, style="ent")
 
 def request_data(message):
     print("You triggered request_data")
@@ -11,6 +20,7 @@ def request_data(message):
 def play_music(message):
     print("You triggered play_music!")
     print(message)
+    named_entity_recoqnition(message)
     # Extract song name from user input
     # song_name = extract_song_name(user_input)
     # Play song using YouTube API
