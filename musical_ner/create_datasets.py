41e7e1b4dafacc79c02b3350d93ea0a27069a6f4
umilic16 <urke.1998ds@gmail.com>
2023-03-21 20:50:31 +0100

    cleaning dataset for NER, added wiki data

--- musical_ner/create_datasets.py
+++ musical_ner/create_datasets.py
@@ -1,9 +1,12 @@
 import os
 import csv
 import re
+import numpy as np
 
 # specify which columns to extract and their corresponding output file names
-columns_to_extract = {'artist': 'artist', 'producer': 'artist', 'writer': 'artist', 'album': 'album', 'genre': 'genre', 'subgenre': 'genre', 'song': 'song'}
+columns_to_extract = {'artist': 'artists', 'producer': 'artists', 'writer': 'artists',
+                      'album': 'albums', 'genre': 'genres', 'subgenre': 'genres', 'song': 'songs'}
+
 
 def create_output_files():
     # create output files and write headers
@@ -28,68 +31,110 @@ def write_outputs(folderpath, filename):
                         columns.append(col)
                 # Extract columns and write them to output files
                 for col in columns:
-                    with open(f'data/{columns_to_extract[col]}.csv', 'a', newline='') as output_file:
+                    with open(f'data/{columns_to_extract[col]}.csv', 'a', newline='', encoding='utf-8') as output_file:
                         writer = csv.writer(output_file)
                         for row in data:
                             if col in row:
                                 writer.writerow([row[col]])
-    except:
-        pass
+    except Exception as ex:
+        print(ex)
+
 
 def remove_duplicates(folder_path, filename):
-    # loop through each CSV file in the folder
     try:
         if filename.endswith('.csv'):
-            with open(os.path.join(folder_path, filename), 'r') as csvfile:
+            with open(os.path.join(folder_path, filename), 'r', encoding='utf-8') as csvfile:
                 reader = csv.reader(csvfile)
                 # read the header row
-                header = next(reader)  
+                header = next(reader)
                 rows = []
+                # data = list(reader)
                 for row in reader:
-                    rows.append(row)
+                    # check if the row is not empty
+                    rows.append([row[0].lower()])
                 unique_rows = list(set(tuple(row) for row in rows))
-            with open(os.path.join(folder_path, f'{filename}'), 'w', newline='') as csvfile:
+                # print(f'{filename} sorted')
+            with open(os.path.join(folder_path, f'{filename}'), 'w', newline='', encoding='utf-8') as csvfile:
                 writer = csv.writer(csvfile)
                 writer.writerow(header)
                 for row in unique_rows:
                     writer.writerow(row)
-    except:
-        pass
-     
+                # print(f'{filename} written')
+    except Exception as ex:
+        print(ex)
+
+
 def split_multiple_genres():
-    with open('data/genre.csv', 'r', newline='') as csvfile:
-        reader = csv.reader(csvfile)
-        unique_rows = []
-        header = next(reader)
-        for row in reader:
-            if ',' in row[0]:
-                genres = [genre.strip() for genre in re.split(r"[,/]",row[0])]
-                # print(genres)
-                for genre in genres:
-                    unique_rows.append(genre)
-            else:
-                unique_rows.append(row[0])
-        # print(unique_rows[:50])
-    # write unique rows back to file
-    with open('data/genre.csv', 'w', newline='') as csvfile:
+    try:
+        with open('data/genres.csv', 'r', newline='') as csvfile:
+            reader = csv.reader(csvfile)
+            unique_rows = []
+            header = next(reader)
+            for row in reader:
+                if ',' in row[0]:
+                    genres = [genre.strip() for genre in re.split(r"[,/]", row[0])]
+                    # print(genres)
+                    for genre in genres:
+                        unique_rows.append(genre)
+                else:
+                    unique_rows.append(row[0])
+            unique_rows.sort()
+        # write unique rows back to file
+        with open('data/genres.csv', 'w', newline='') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(header)
+            for row in unique_rows:
+                writer.writerow([row.lower()])
+    except Exception as ex:
+        print(ex)
+
+# # set the path to the folder containing CSV files
+# create_output_files()
+
+# # write all columns needed from music/data
+# folder_path = 'data/keggle_data/'
+# for filename in os.listdir(folder_path):
+#     write_outputs(folder_path, filename)
+
+# # remove duplicates from exported files
+# folder_path = 'data/'
+# for filename in os.listdir(folder_path):
+#     remove_duplicates(folder_path, filename)
+
+# # split rows that contain multiple values in genres.csv and remove duplicates again
+# split_multiple_genres()
+# remove_duplicates(folder_path, 'genres.csv')
+
+# ultra slow and not needed function, used once to clean up wiki artist entries
+def clean_up_artists():
+    # remove_duplicates('data/', 'wiki_artists.csv')
+    data = []
+    with open('data/wiki_data/wiki_artists.csv', 'r', encoding='utf-8') as wiki_file, \
+        open('data/genres.csv', 'r', encoding='utf-8') as genre_file, \
+        open('data/instruments.csv', 'r', encoding='utf-8') as insturment_file:
+        wiki_reader = csv.reader(wiki_file)
+        genre_reader = csv.reader(genre_file)
+        instrment_reader = csv.reader(insturment_file)
+        next(genre_reader)
+        next(instrment_reader)
+        header = next(wiki_reader)
+        genres = np.array([row[0] for row in genre_reader])
+        instruments = np.array([row[0] for row in instrment_reader])
+        group = np.concatenate((genres,instruments))
+        # print(group)
+        for row in wiki_reader:
+            artist = row[0].lower()
+            for entry in group:
+                if entry not in artist:
+                    data.append(row)
+        print(len(data))
+    with open('data/wiki_data/wiki_artists.csv', 'w', encoding='utf-8', newline='') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(header)
-        for row in unique_rows:
+        for row in data:
             writer.writerow([row])
 
-# set the path to the folder containing CSV files
-create_output_files()
 
-# write all columns needed from music/data
-folder_path = 'data/music_data/'
-for filename in os.listdir(folder_path):
-    write_outputs(folder_path, filename)
-
-# remove duplicates from exported files
-folder_path = 'data/'
-for filename in os.listdir(folder_path):
-    remove_duplicates(folder_path, filename)
-
-# split rows that contain multiple values in genres.csv and remove duplicates again
-split_multiple_genres()
-remove_duplicates(folder_path, 'genre.csv')
+# folder_path = 'data/'
+# remove_duplicates(folder_path, 'artists.csv')
+clean_up_artists()
---
+++ musical_ner/wiki_fetch.py
@@ -0,0 +1,124 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+import re
+
+# this functions just gets all text from genres page, i formated it manually using regex
+
+
+def get_all_genres():
+    try:
+        # URL of the Wikipedia page with the list of music artists
+        url = "https://en.wikipedia.org/wiki/List_of_music_genres_and_styles"
+        # Send a GET request to the URL and retrieve the HTML content
+        response = requests.get(url)
+        html_content = response.content
+        # print(html_content)
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(html_content, "lxml")
+        uls = soup.find_all("li")
+        genres = []
+        for ul in uls:
+            line = str(ul.text)
+            genres.append(line)
+        # save genres, print, whatever
+    except:
+        pass
+
+# this function gets all artists from all wiki pages in the lists of musicians page
+
+
+def get_all_artists():
+    try:
+        # URL of the Wikipedia page with the list of music artists
+        url = "https://en.wikipedia.org/wiki/Lists_of_musicians"
+        # Send a GET request to the URL and retrieve the HTML content
+        response = requests.get(url)
+        html_content = response.content
+        # print(html_content)
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(html_content, "lxml")
+        links = []
+        # get all links for artist lists
+        for a in soup.find_all('a', href=True):
+            result = re.search(r"/wiki/List_of_.+", a['href'])
+            if result:
+                # print(result)
+                links.append(result.string)
+        # print(links)
+        # collect every artist from every link
+        for link in links:
+            url = f"https://en.wikipedia.org/{link}"
+            # print(f'contacted link {link}')
+            response = requests.get(url)
+            html_content = response.content
+            soup = BeautifulSoup(html_content, "lxml")
+            uls = soup.find_all("li")
+
+            found_start = False
+            found_end = False
+            text = []
+            # the uls that contain artist names have no ids but the ul before the artist names and after it has an id
+            # so we go through the uls in between those
+            for ul in uls:
+                # Skip ul elements until we find the first ul without an id
+                if not found_start and ul.has_attr('id'):
+                    continue
+                else:
+                    found_start = True
+                # Stop saving ul elements once we find the next ul with an id or class
+                if ul.has_attr("id") or ul.has_attr('class'):
+                    if not found_end:
+                        found_end = True
+                        break
+                    else:
+                        break
+                line = str(ul.text)
+                if len(line) > 1:
+                    text.append(line)
+            # save the artists into csv
+            with open("data/wiki_data/wiki_artists.csv", "w", newline='', encoding="utf-8") as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(['artist'])
+                print(f'writing data from {link}')
+                for line in text:
+                    writer.writerow([line])
+    except Exception as ex:
+        print(ex)
+
+# get_all_artists()
+
+
+def get_all_instruments():
+    try:
+        # URL of the Wikipedia page with the list of insturments
+        url = "https://en.wikipedia.org/wiki/List_of_musical_instruments"
+        # Send a GET request to the URL and retrieve the HTML content
+        response = requests.get(url)
+        html_content = response.content
+        # print(html_content)
+        # Parse the HTML content with BeautifulSoup
+        soup = BeautifulSoup(html_content, "lxml")
+        tables = soup.find_all('table', class_='wikitable')
+        data = []
+        for table in tables:
+            if table is not None:
+                rows = table.find_all('tr')
+                # print(rows)
+                for row in rows:
+                    if row is not None:
+                        instrument = row.find('td')
+                        # print(type(instrument))
+                        if instrument is not None:
+                            data.append(instrument.text.strip())
+        # save the instruments into csv
+        with open("data/instruments.csv", "w", newline='', encoding="utf-8") as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(['instrument'])
+            for row in data:
+                writer.writerow([row.lower()])
+    except Exception as ex:
+        print(ex)
+
+
+get_all_instruments()
